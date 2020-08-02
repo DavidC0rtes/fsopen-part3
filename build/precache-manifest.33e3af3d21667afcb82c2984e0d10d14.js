1fb8df2b9a068b6e4da710b9be61e4896730ba54
davidc0rtes <david.cortes@correounivalle.edu.co>
2020-08-01 21:51:04 -0500

    production build.

---
+++ build/precache-manifest.33e3af3d21667afcb82c2984e0d10d14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec169512eb67740f69df956543851bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cab2e1e853fb59f8d2f",
+    "url": "/static/js/main.67d91996.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
